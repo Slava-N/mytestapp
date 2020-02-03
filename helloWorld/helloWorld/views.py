4a99e80606c3d42dcb7a420944d4cebbca990a7a
slava-n <novozhilovu@gmail.com>
2020-02-04 00:09:26 +0300

    added html template

--- helloWorld/helloWorld/views.py
+++ helloWorld/helloWorld/views.py
@@ -1,2 +1,6 @@
 from django.views import generic
 from django.shortcuts import render
+
+from django.shortcuts import render
+def post_list(request):
+    return render(request, 'helloWorldTemplate/post_list.html', {})
